0df3a9b1e515165cdd4d483edcb26fdfbcb4b333
vtnedorost <Sindicase@seznam.cz>
2023-03-21 16:35:31 +0100

    update - not working

--- AbbyWeb/Pages/Admin/MenuItems/Index.cshtml.cs
+++ AbbyWeb/Pages/Admin/MenuItems/Index.cshtml.cs
@@ -1,5 +1,3 @@
-using Abby.DataAcess.Repository.IRepository;
-using Abby.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,21 +5,8 @@ namespace AbbyWeb.Pages.Admin.MenuItems
 {
     public class IndexModel : PageModel
     {
-
-        private readonly IUnitOfWork _unitOfWOrk;
-
-
-        public IEnumerable<MenuItem> MenuItems { get; set; }
-
-
-        public IndexModel(IUnitOfWork unitOfWork)
-        {
-            _unitOfWOrk = unitOfWork;
-        }
-
         public void OnGet()
         {
-            MenuItems = _unitOfWOrk.MenuItem.GetAll();
         }
     }
 }
--- AbbyWeb/wwwroot/js/menuItem.js
+++ AbbyWeb/wwwroot/js/menuItem.js
@@ -1,22 +1,29 @@
-﻿
-$(document).ready(function () {
-	$('#DT_load').DataTable({
-		"ajax": {
-			"url": "/api/MenuItem",
-			"type": "GET",
-			"datatype": "json"
+﻿$(document).ready(function () {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/MenuItem",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" >
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-success text-white mx-2">
+                            <i class="bi bi-pencil-square"></i>  </a>
+                            <a href="/Admin/MenuItems/upsert?id${data}"  class="btn btn-danger text-white mx-2">
+                             <i class="bi bi-trash-fill"></i>  </a>
+                            </div>`
+                },
 
-		},
-		"columns": [
-			{ "data": "name", "width": "25%" },
-			{ "data": "price", "width": "25%" },
-			{ "data": "category.name", "width": "15%" },
-			{ "data": "foodType.name", "width": "15%" }
-
-
-		],
-		"width": "100%"
-
-
-	});
+                "width": "15%"
+            }
+        ],
+        "width": "100%"
+    });
 });
